b1fe34d1f95795e8b8097f7569d30c1efb9d9e98
Tapio Vaattanen <vaattanen@gmail.com>
2016-09-12 19:40:45 -0400

    Edited run.sh

--- MongoDB/run.sh
+++ MongoDB/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 # *********************************** #
 # Script to test Shard with MongoDB   #
@@ -16,35 +16,35 @@ MONGOHOST=quelimane
 # Start config servers
 
 for i in {0..2}
-        do
-                if ! [ -d cfg${i} ]
-                        then
-                                mkdir cfg${i}
-				echo mongod --configsvr --dbpath cfg${i} --port 2605$i --fork --logpath log.cfg${i} --logappend
-				mongod --configsvr --dbpath cfg${i} --port 2605$i --fork --logpath log.cfg${i} --logappend >> $LOG
-                        else
-                                echo "Directory cfg${i} exists, skipping..."
-                fi
+do
+    if ! [ -d cfg${i} ]
+    then
+        mkdir cfg${i}
+        echo mongod --configsvr --dbpath cfg${i} --port 2605$i --fork --logpath log.cfg${i} --logappend
+        mongod --configsvr --dbpath cfg${i} --port 2605$i --fork --logpath log.cfg${i} --logappend >> $LOG 
+    else
+        echo "Directory cfg${i} exists, skipping..."
+    fi
 done
 
 # Start the mongods
 
 k=0
 for i in {a,b,c,d}
-	do
-		let k++
-		for j in {0..2}
-			do
-				if ! [ -d $i$j ]
-					then 
-						mkdir $i$j
-						echo mongod --shardsvr --replSet $i --dbpath $i$j --logpath log.$i$j --port 27${k}0${j} --fork --logappend --smallfiles --oplogSize 50
-						mongod --shardsvr --replSet $i --dbpath $i$j --logpath log.$i$j --port 27${k}0${j} --fork --logappend --smallfiles --oplogSize 50 >> $LOG
-					else
-						echo "Directory $i$j exist, skipping..."
-				fi
-			
-	done
+do
+    let k++
+    for j in {0..2}
+    do
+        if ! [ -d $i$j ]
+        then
+            mkdir $i$j
+            echo mongod --shardsvr --replSet $i --dbpath $i$j --logpath log.$i$j --port 27${k}0${j} --fork --logappend --smallfiles --oplogSize 50
+            mongod --shardsvr --replSet $i --dbpath $i$j --logpath log.$i$j --port 27${k}0${j} --fork --logappend --smallfiles --oplogSize 50 >> $LOG 
+        else
+            echo "Directory $i$j exist, skipping..."
+        fi
+
+    done
 done
 
 
@@ -54,7 +54,7 @@ echo mongos --configdb $MONGOHOST:26050,$MONGOHOST:26051,$MONGOHOST:26052 --fork
 mongos --configdb $MONGOHOST:26050,$MONGOHOST:26051,$MONGOHOST:26052 --fork --logappend --logpath log.mongos0 >> $LOG
 
 for i in {1..3}
-	do
-		echo mongos --configdb $MONGOHOST:26050,$MONGOHOST:26051,$MONGOHOST:26052 --fork --logappend --logpath log.mongos${i} --port 2606${i}
-		mongos --configdb $MONGOHOST:26050,$MONGOHOST:26051,$MONGOHOST:26052 --fork --logappend --logpath log.mongos0 --port 2606${i} >> $LOG
+do
+    echo mongos --configdb $MONGOHOST:26050,$MONGOHOST:26051,$MONGOHOST:26052 --fork --logappend --logpath log.mongos${i} --port 2606${i}
+    mongos --configdb $MONGOHOST:26050,$MONGOHOST:26051,$MONGOHOST:26052 --fork --logappend --logpath log.mongos0 --port 2606${i} >> $LOG
 done

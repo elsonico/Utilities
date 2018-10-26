5b9bc69491424d02cd37f9375883a19948801fb0
tapio.vaattanen <tapio.vaattanen@ethoca.com>
2018-10-26 12:10:05 -0400

    Obfuscating credentials unsafe way.

--- db2-utils/db2_test.py
+++ db2-utils/db2_test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+pwd = base64.b64decode("TWFyaWEjMTJTYW56")#!/usr/bin/env python
 """ Testing connecting to DB2  utilizing pyodbc.
 
     AUTHOR: Tapio Vaattanen
@@ -10,23 +10,25 @@
 import sys
 import pyodbc
 import pandas as pd
+import base64
 from db2_utils import DB2Utils
 
 
 class DB2Test(object):
     """
-    This Class has basic set of DB2 Utilities.
+    This Class is meant for simple testing DB2Utils.
     """
     def __init__(self, output_file):
         self.output_file = output_file
 
     def main(self):
         dsn = 'sample'
-        uid = 'db2eth'
-        pwd = 'ibm4es'
+        # Obfuscating the credentials with totally unsafe way.
+        uid = base64.b64decode("ZGIyaW5zdDE=")
+        pwd = base64.b64decode("'cGFzc3dvcmQ=")
         db2_utils = DB2Utils(dsn, uid, pwd)
         db2_connection = db2_utils.db2_connect()
-        query = "select tabname from syscat.tables"
+        query = "select tabname from syscat.tables where tabschema = 'db2inst1'"
         df = db2_utils.write_to_df(db2_connection, query)
         db2_utils.write_to_stdout(db2_connection, query)
         db2_connection.close()
@@ -36,7 +38,7 @@ class DB2Test(object):
             print("Error writing to  output file: %s" % err)
             sys.exit(1)
         for index, row in df.iterrows():
-            sql_file.write("Table name: " + df.iloc[index,0] + "\n")
+            sql_file.write("Table name: " + df.iloc[index, 0] + "\n")
 
 
 if __name__ == '__main__':
